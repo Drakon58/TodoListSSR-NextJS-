257361de802288a864d1ebd40aa05286d10e2d1f
Daniel <widowmaker58@live.ca>
2020-05-05 17:06:46 -0400

    Removing some test code used for learning NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Link from 'next/link';
-
-import { Test } from '../src/util/Test';
-
 import TodoList from '../src/component/TodoList.component';
 import NavHeader from '../src/component/nav/NavHeader.component'
 import { Todo } from '../src/util/Todo';
@@ -18,27 +14,9 @@ const Index = (props) => {
       <div>
         <NavHeader />
         <TodoList GetTodo={Todo.GetTodo} RemoveTodo={Todo.RemoveTodo} />
-        <ul>
-          {
-            props.shows.map(show => (
-              <li key={show.id}>
-                <Link href="/edit/[id]" as={`/edit/${show.id}`}>
-                  <a>{show.name}</a>
-                </Link>
-              </li>
-            ))}
-        </ul>
       </div>
     </>
   )
-}
-
-Index.getInitialProps = async () => {
-  let entries = await Test.GetTVAmaze();
-
-  return {
-    shows: entries.map(entry => entry.show)
-  };
 }
 
 export default Index;
